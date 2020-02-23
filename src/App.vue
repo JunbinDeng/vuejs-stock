45031e4ca7f6e29d390be903d427c008c9e76811
Junbin Deng <junbin.deng@gmail.com>
2020-02-23 22:00:02 +0800

    feat: stock trader main functions

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header";
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import 'bootstrap/dist/js/bootstrap.min.js'
+
+  export default {
+    components: {
+      appHeader: Header
+    },
+    created() {
+      this.$store.dispatch('initStocks');
+    }
+  }
+</script>
+
+<style>
+  body {
+    padding: 30px;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" class="nav-link" tag="li"><a>Portfolio</a></router-link>
+        <router-link to="/stocks" class="nav-link" tag="li"><a>Stocks</a></router-link>
+      </ul>
+      <div class="form-inline my-2 my-lg-0">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#" @click="endDay">End Day</a>
+        </div>
+        <div class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">Save & Load</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </div>
+        <strong class="navbar-text navbar-right">Funds: {{funds | currency}}</strong>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  import {mapActions} from 'vuex';
+
+  export default {
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      }
+    },
+    methods: {
+      ...mapActions([
+        'randomizeStocks'
+      ]),
+      endDay() {
+        this.randomizeStocks();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
